f2dd2ac4a1e5b6822bcf4a35c6f8c0d90aa47037
vnam <namvuhuong@gmail.com>
2019-04-01 20:47:32 +0700

    18done

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,3 +1,381 @@
+// const alpha1_alpha3 = {
+//     aa: 'aar',
+//     ab: 'abk',
+//     af: 'afr',
+//     ak: 'aka',
+//     am: 'amh',
+//     ar: 'ara',
+//     an: 'arg',
+//     as: 'asm',
+//     av: 'ava',
+//     ae: 'ave',
+//     ay: 'aym',
+//     az: 'aze',
+//     ba: 'bak',
+//     bm: 'bam',
+//     be: 'bel',
+//     bn: 'ben',
+//     bh: 'bih',
+//     bi: 'bis',
+//     bo: 'bod',
+//     bs: 'bos',
+//     br: 'bre',
+//     bg: 'bul',
+//     ca: 'cat',
+//     cs: 'ces',
+//     ch: 'cha',
+//     ce: 'che',
+//     cu: 'chu',
+//     cv: 'chv',
+//     kw: 'cor',
+//     co: 'cos',
+//     cr: 'cre',
+//     cy: 'cym',
+//     da: 'dan',
+//     de: 'deu',
+//     dv: 'div',
+//     dz: 'dzo',
+//     el: 'ell',
+//     en: 'eng',
+//     eo: 'epo',
+//     et: 'est',
+//     eu: 'eus',
+//     ee: 'ewe',
+//     fo: 'fao',
+//     fa: 'fas',
+//     fj: 'fij',
+//     fl: 'fil',
+//     fi: 'fin',
+//     fr: 'fra',
+//     fy: 'fry',
+//     ff: 'ful',
+//     gd: 'gla',
+//     ga: 'gle',
+//     gl: 'glg',
+//     gv: 'glv',
+//     gn: 'grn',
+//     gu: 'guj',
+//     ht: 'hat',
+//     ha: 'hau',
+//     sh: 'hbs',
+//     he: 'heb',
+//     hz: 'her',
+//     hi: 'hin',
+//     ho: 'hmo',
+//     hr: 'hrv',
+//     hu: 'hun',
+//     hy: 'hye',
+//     ig: 'ibo',
+//     io: 'ido',
+//     ii: 'iii',
+//     iu: 'iku',
+//     ie: 'ile',
+//     ia: 'ina',
+//     id: 'ind',
+//     ik: 'ipk',
+//     is: 'isl',
+//     it: 'ita',
+//     jv: 'jav',
+//     ja: 'jpn',
+//     kl: 'kal',
+//     kn: 'kan',
+//     ks: 'kas',
+//     ka: 'kat',
+//     kr: 'kau',
+//     kk: 'kaz',
+//     km: 'khm',
+//     ki: 'kik',
+//     rw: 'kin',
+//     ky: 'kir',
+//     kv: 'kom',
+//     kg: 'kon',
+//     ko: 'kor',
+//     kj: 'kua',
+//     ku: 'kur',
+//     lo: 'lao',
+//     la: 'lat',
+//     lv: 'lav',
+//     li: 'lim',
+//     ln: 'lin',
+//     lt: 'lit',
+//     lb: 'ltz',
+//     lu: 'lub',
+//     lg: 'lug',
+//     mh: 'mah',
+//     ml: 'mal',
+//     mr: 'mar',
+//     mk: 'mkd',
+//     mg: 'mlg',
+//     mt: 'mlt',
+//     mn: 'mon',
+//     mi: 'mri',
+//     ms: 'msa',
+//     my: 'mya',
+//     na: 'nau',
+//     nv: 'nav',
+//     nr: 'nbl',
+//     nd: 'nde',
+//     ng: 'ndo',
+//     ne: 'nep',
+//     nl: 'nld',
+//     nn: 'nno',
+//     nb: 'nob',
+//     no: 'nor',
+//     ny: 'nya',
+//     oc: 'oci',
+//     oj: 'oji',
+//     or: 'ori',
+//     om: 'orm',
+//     os: 'oss',
+//     pa: 'pan',
+//     pi: 'pli',
+//     pl: 'pol',
+//     pt: 'por',
+//     ps: 'pus',
+//     qu: 'que',
+//     rm: 'roh',
+//     ro: 'ron',
+//     rn: 'run',
+//     ru: 'rus',
+//     sg: 'sag',
+//     sa: 'san',
+//     si: 'sin',
+//     sk: 'slk',
+//     sl: 'slv',
+//     se: 'sme',
+//     sm: 'smo',
+//     sn: 'sna',
+//     sd: 'snd',
+//     so: 'som',
+//     st: 'sot',
+//     es: 'spa',
+//     sq: 'sqi',
+//     sc: 'srd',
+//     sr: 'srp',
+//     ss: 'ssw',
+//     su: 'sun',
+//     sw: 'swa',
+//     sv: 'swe',
+//     ty: 'tah',
+//     ta: 'tam',
+//     tt: 'tat',
+//     te: 'tel',
+//     tg: 'tgk',
+//     tl: 'tgl',
+//     th: 'tha',
+//     ti: 'tir',
+//     to: 'ton',
+//     tn: 'tsn',
+//     ts: 'tso',
+//     tk: 'tuk',
+//     tr: 'tur',
+//     tw: 'twi',
+//     ug: 'uig',
+//     uk: 'ukr',
+//     ur: 'urd',
+//     uz: 'uzb',
+//     ve: 'ven',
+//     vi: 'vie',
+//     vo: 'vol',
+//     wa: 'wln',
+//     wo: 'wol',
+//     xh: 'xho',
+//     yi: 'yid',
+//     yo: 'yor',
+//     za: 'zha',
+//     zh: 'zho',
+//     zu: 'zul',
+// }
+//
+// alpha3_alpha1 = {
+//     aar: 'aa',
+//     abk: 'ab',
+//     afr: 'af',
+//     aka: 'ak',
+//     amh: 'am',
+//     ara: 'ar',
+//     arg: 'an',
+//     asm: 'as',
+//     ava: 'av',
+//     ave: 'ae',
+//     aym: 'ay',
+//     aze: 'az',
+//     bak: 'ba',
+//     bam: 'bm',
+//     bel: 'be',
+//     ben: 'bn',
+//     bih: 'bh',
+//     bis: 'bi',
+//     bod: 'bo',
+//     bos: 'bs',
+//     bre: 'br',
+//     bul: 'bg',
+//     cat: 'ca',
+//     ces: 'cs',
+//     cha: 'ch',
+//     che: 'ce',
+//     chu: 'cu',
+//     chv: 'cv',
+//     cor: 'kw',
+//     cos: 'co',
+//     cre: 'cr',
+//     cym: 'cy',
+//     dan: 'da',
+//     deu: 'de',
+//     div: 'dv',
+//     dzo: 'dz',
+//     ell: 'el',
+//     eng: 'en',
+//     epo: 'eo',
+//     est: 'et',
+//     eus: 'eu',
+//     ewe: 'ee',
+//     fao: 'fo',
+//     fas: 'fa',
+//     fij: 'fj',
+//     fil: 'fl',
+//     fin: 'fi',
+//     fra: 'fr',
+//     fry: 'fy',
+//     ful: 'ff',
+//     gla: 'gd',
+//     gle: 'ga',
+//     glg: 'gl',
+//     glv: 'gv',
+//     grn: 'gn',
+//     guj: 'gu',
+//     hat: 'ht',
+//     hau: 'ha',
+//     hbs: 'sh',
+//     heb: 'he',
+//     her: 'hz',
+//     hin: 'hi',
+//     hmo: 'ho',
+//     hrv: 'hr',
+//     hun: 'hu',
+//     hye: 'hy',
+//     ibo: 'ig',
+//     ido: 'io',
+//     iii: 'ii',
+//     iku: 'iu',
+//     ile: 'ie',
+//     ina: 'ia',
+//     ind: 'id',
+//     ipk: 'ik',
+//     isl: 'is',
+//     ita: 'it',
+//     jav: 'jv',
+//     jpn: 'ja',
+//     kal: 'kl',
+//     kan: 'kn',
+//     kas: 'ks',
+//     kat: 'ka',
+//     kau: 'kr',
+//     kaz: 'kk',
+//     khm: 'km',
+//     kik: 'ki',
+//     kin: 'rw',
+//     kir: 'ky',
+//     kom: 'kv',
+//     kon: 'kg',
+//     kor: 'ko',
+//     kua: 'kj',
+//     kur: 'ku',
+//     lao: 'lo',
+//     lat: 'la',
+//     lav: 'lv',
+//     lim: 'li',
+//     lin: 'ln',
+//     lit: 'lt',
+//     ltz: 'lb',
+//     lub: 'lu',
+//     lug: 'lg',
+//     mah: 'mh',
+//     mal: 'ml',
+//     mar: 'mr',
+//     mkd: 'mk',
+//     mlg: 'mg',
+//     mlt: 'mt',
+//     mon: 'mn',
+//     mri: 'mi',
+//     msa: 'ms',
+//     mya: 'my',
+//     nau: 'na',
+//     nav: 'nv',
+//     nbl: 'nr',
+//     nde: 'nd',
+//     ndo: 'ng',
+//     nep: 'ne',
+//     nld: 'nl',
+//     nno: 'nn',
+//     nob: 'nb',
+//     nor: 'no',
+//     nya: 'ny',
+//     oci: 'oc',
+//     oji: 'oj',
+//     ori: 'or',
+//     orm: 'om',
+//     oss: 'os',
+//     pan: 'pa',
+//     pli: 'pi',
+//     pol: 'pl',
+//     por: 'pt',
+//     pus: 'ps',
+//     que: 'qu',
+//     roh: 'rm',
+//     ron: 'ro',
+//     run: 'rn',
+//     rus: 'ru',
+//     sag: 'sg',
+//     san: 'sa',
+//     sin: 'si',
+//     slk: 'sk',
+//     slv: 'sl',
+//     sme: 'se',
+//     smo: 'sm',
+//     sna: 'sn',
+//     snd: 'sd',
+//     som: 'so',
+//     sot: 'st',
+//     spa: 'es',
+//     sqi: 'sq',
+//     srd: 'sc',
+//     srp: 'sr',
+//     ssw: 'ss',
+//     sun: 'su',
+//     swa: 'sw',
+//     swe: 'sv',
+//     tah: 'ty',
+//     tam: 'ta',
+//     tat: 'tt',
+//     tel: 'te',
+//     tgk: 'tg',
+//     tgl: 'tl',
+//     tha: 'th',
+//     tir: 'ti',
+//     ton: 'to',
+//     tsn: 'tn',
+//     tso: 'ts',
+//     tuk: 'tk',
+//     tur: 'tr',
+//     twi: 'tw',
+//     uig: 'ug',
+//     ukr: 'uk',
+//     urd: 'ur',
+//     uzb: 'uz',
+//     ven: 've',
+//     vie: 'vi',
+//     vol: 'vo',
+//     wln: 'wa',
+//     wol: 'wo',
+//     xho: 'xh',
+//     yid: 'yi',
+//     yor: 'yo',
+//     zha: 'za',
+//     zho: 'zh',
+//     zul: 'zu',
+// }
+
 function showHidePassword() {
     var x = document.getElementById("userPW");
     if (x.type === "password") {
@@ -10,55 +388,82 @@ function showHidePassword() {
     }
 }
 
+var offset;
 
+function update_heritage_data(article_container_clone, photo) {
+    // Display data to index.html
+    // Heritage photograph
+    article_container_clone.find('.post__img').attr('src', 'https:' + photo['image_url'] + '?size=medium');
+    // User avatar
+    article_container_clone.find('.user-avt').attr("src", 'https:' + photo['account']['picture_url']);
+    // Caption
+    article_container_clone.find('.info__caption--text').text(photo['title'][0]['content']);
+    // Area user local
+    article_container_clone.find('.location__name').text(photo['area_name']);
+    // Taken year
+    article_container_clone.find('.year--canchange').text(photo['capture_time'] || 'N/A');
+    article_container_clone.removeAttr('id');
+    article_container_clone.show();
+    return article_container_clone
+}
 
-$(document).ready(function () {
-    mHeritageGoService.getPhotos({ limit: 5 }).then( photos => {
-        $article_container = $('#post-container');
+article_container = $('#post-container');
+article_container.hide();
+function update_content(selected_section, limit, {
+    offset = 0
+}) {
+    mHeritageGoService.getPhotos({
+        limit: limit,
+        offset: offset
+    }).then(function (photos) {
         $(photos).each(function () {
-            mHeritageGoService.getPhoto(this).then(photo => {
-                var $article_container_clone = $article_container.clone();
-                // console.log($article_container_clone);
-                // User avatar
-                $article_container_clone.find('.user-avt').attr('src', 'https:' + photo.account.picture_url);
-                // Caption name
-                $article_container_clone.find('.info__caption--text').text(photo.title[0].content);
-                // Location name
-                $article_container_clone.find('.location__name').text(photo.area_name);
-                // Capture time
-                $article_container_clone.find('.year--canchange').text(photo.capture_time || 'N/a');
-                // Heritage photo
-                $article_container_clone.find('.post__img').attr('src', 'https:' + photo.image_url + "?size=medium");
-                $article_container_clone.removeAttr('id');
-                $('.posts').append($article_container_clone);
+            // Retrieve detail of a photo
+            mHeritageGoService.getPhoto(this).then(function (photo) {
+                var article_container_clone = article_container.clone();
+                article_container_clone = update_heritage_data(article_container_clone, photo);
+                selected_section.append(article_container_clone);
             });
         })
-    }).catch(function (error) {console.log(error)})
-})
+    }).catch(error => {
+        console.log(error);
+    });
+};
+
+// Ready function to load code
+$(document).ready(function () {
+    // Get photos form HeritageService
+    limit = 5;
+    update_content($('.posts'), limit, {offset: offset});
+    offset = limit;
+});
+
+$(window).scroll(function () {
+    // Update content when scroll down to the end (Need refactoring)
+    if ($(window).scrollTop() + $(window).height() >= $(document).height()) {
+        limit = 3;
+        update_content($('.posts'), limit, {
+            offset: offset
+        });
+        offset += limit;
+    }
+
+});
 
 
+// function getPrefferedLanguages() {
+//     var preffered_languages = navigator.languages;
+//     return preffered_languages.slice(preffered_languages.length / 2, preffered_languages.length)
+// }
+//
+// function showInput(header_translate) {
+//     alert(header_translate);
+// }
 
 window.addEventListener('scroll', function () {
   document.body.classList[
     window.scrollY > 20 ? 'add': 'remove'
   ]('scrolled');
 });
-
-
-// var content = $('.site-content'),
-//     header = $('.site-header');
-
-// $(content).clone().prependTo(header).addClass('blurred');
-// $(document).scroll(function(){
-//   var _scroll = $(this).scrollTop();
-
-//   $('.blurred').css({
-//     '-webkit-transform' : 'translateY(-'+_scroll+'px)',
-//     'transform' : 'translateY(-'+_scroll+'px)',
-
-//   });
-
-// })
 
 var content = $('.content'),
     header = $('.header-float');
